32a078fe2b5c3a1f1da5a259671cb46de07ce2be
Oliver Dahl <oliverpdahl@gmail.com>
2020-03-26 15:55:28 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     if params[:name].nil?
       session.clear
-      redirect_to '/login'
+      redirect_to controller: 'sessions', action: 'new'
     end
   end
 end
